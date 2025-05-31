c9b14dedef7c304de1a04fc96f3338b2d4127bf6
Maria-Nazar1 <mnazar5@abc.gob.ar>
2025-05-30 22:42:25 -0300

    feat: Implementación inicial del CRUD de Empleados

---
+++ src/models/Empleado.js
@@ -0,0 +1,29 @@
+// src/models/Empleado.js
+
+class Empleado {
+    constructor(
+        empleadoID, // Identificador único para el empleado
+        nombre,
+        apellido,
+        dni, // DNI o documento de identidad
+        correoElectronico,
+        telefono,
+        usuario, // Nombre de usuario para el login
+        contrasena, // Contraseña (¡IMPORTANTE! Idealmente, debería ser hasheada, no guardada en texto plano)
+        rol, // Rol del empleado (ej. 'Administrador', 'Empleado', 'Supervisor', 'Agente Inmobiliario', 'Contador')
+        foto = null // URL o ruta a la foto del empleado, opcional
+    ) {
+        this.empleadoID = empleadoID;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.correoElectronico = correoElectronico;
+        this.telefono = telefono;
+        this.usuario = usuario;
+        this.contrasena = contrasena; // Considerar hashing en un entorno real
+        this.rol = rol;
+        this.foto = foto;
+    }
+}
+
+export default Empleado;
